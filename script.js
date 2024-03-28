caba10e5b3ffc44982a0482ef6584b4e072dee13
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2024-03-28 22:39:02 +0530

    car game

--- script.js
+++ script.js
@@ -44,6 +44,59 @@ function keyReleased(eventDetails){
 }
 
 
+// move Lines 
+
+function moveLines(){
+    let lines = document.querySelectorAll('.line');
+    for(let t of lines){
+    //    console.log(t.y);
+        if(t.y >=662){
+            t.y = t.y - 750;
+        }
+
+        t.y = t.y + player.speed; // 5px
+        t.style.top = t.y + "px";
+    }
+
+}
+
+
+function isCollide(a,b){
+  let aRect = a.getBoundingClientRect();
+  let bRect = b.getBoundingClientRect();
+
+
+  return !(
+         (aRect.bottom < bRect.top) || (aRect.top > bRect.bottom) || (aRect.right < bRect.left) || (aRect.left > bRect.right) // discuss again
+         )
+}
+
+
+function moveEnemy(car){
+
+    let enemyies = document.querySelectorAll('.enemy');
+    for(let t of enemyies){
+          
+        if(isCollide(car,t) == true){
+            console.log("Collided");
+            player.start = false;
+        }
+
+
+        if(t.y >= 662){
+            t.y = t.y - 650;
+            t.style.left = parseInt(Math.random()*150) + "px";
+
+        }
+
+        t.y = t.y + player.speed; // 5px
+        t.style.top = t.y + "px";
+    }
+
+}
+
+
+
 // game play logic 
 
 function gamePlay(){
@@ -51,6 +104,8 @@ function gamePlay(){
      let car = document.querySelector('.car');
      let road = gameArea.getBoundingClientRect()
     //  console.log(road);
+    moveLines()
+    moveEnemy(car)
      if(player.start){
         if(keyObj.ArrowUp == true  && road.top < player.y){
             // console.log(road.top, player.y)
@@ -68,6 +123,7 @@ function gamePlay(){
 
         car.style.top = player.y + "px";
         car.style.left = player.x + "px";
+        
         requestAnimationFrame(gamePlay);
      }
     
@@ -92,6 +148,54 @@ function start(){
     player.x = car.offsetLeft;
     player.y = car.offsetTop;
     // console.log("from top",player.y, "from left", player.x);
+    
+    // generate raod divider: 
+
+    for(let x = 0; x<5; x++){
+        let div = document.createElement('div');
+        div.className = 'line';
+        div.y = x*150
+        div.style.top = x*150 + "px";   // 0, 150, 300, 450, 600
+        gameArea.append(div);
+    }
+
+    // generate enemy cars: 
+
+    for(let x = 0 ; x<3; x++){
+        let enemy = document.createElement('div'); 
+        enemy.className = 'enemy';
+        enemy.style.backgroundColor = "red"; 
+        enemy.y = (x+1)*300*-1;  // -300, -600, -900
+        enemy.style.top = enemy.y + "px";
+        enemy.style.left = parseInt(Math.random()*150) + "px";
+        gameArea.append(enemy);
+    }
 }
 
 // 4 controls: up, down, left, right
+
+
+
+
+
+
+
+// placement criterai: 
+// todoApp application => JS and React (15 mins screen share)
+// 24-48 to do a samll projects 
+// Swiggy Home page clone 
+// Amazon Home page clone
+// Twitter Home page clone
+
+
+
+
+
+
+
+
+
+// car a , car b
+
+
+
